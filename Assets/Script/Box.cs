b1ece3515abea4c8fd258a746f15cf3b02890732
Hoang Nong <hoang.nong@ezgames.cc>
2021-03-19 16:58:41 +0700

    Random Generator
    Code random generator

--- Assets/Script/Box.cs
+++ Assets/Script/Box.cs
@@ -1,41 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Box : MonoBehaviour
 {
-    public Box Top { get; set; }
-    public Box Bottom { get; set; }
-    public Box Left { get; set; }
-    public Box Right { get; set; }
 
-    public bool isActiveTop { get; set; }
-    public bool isActiveBottom { get; set; }
-    public bool isActiveLeft { get; set; }
-    public bool isActiveRight { get; set; }
+    private Box top;
+    private Box bottom;
+    private Box left;
+    private Box right;
+
+
+    public Box()
+    {
+        Top = null;
+        Bottom = null;
+        Left = null;
+        Right = null;
+        this.isActiveTop = true;
+        this.isActiveBottom = true;
+        this.isActiveLeft = true;
+        this.isActiveRight = true;
+    }
+
+    public bool isActiveTop;
+    public bool isActiveBottom;
+    public bool isActiveLeft;
+    public bool isActiveRight;
+
+    public bool isCheck = false;
+
+    public Box Top { get => top; set => top = value; }
+    public Box Bottom { get => bottom; set => bottom = value; }
+    public Box Left { get => left; set => left = value; }
+    public Box Right { get => right; set => right = value; }
 
     public void AddTop(Box NextTop)
     {
         if (Top == null && isActiveTop)
+        {
             Top = NextTop;
+            NextTop.Bottom = this;
+            isActiveTop = !isActiveTop;
+            NextTop.isActiveBottom = !NextTop.isActiveBottom;
+        }
+            
     }
 
     public void AddLeft(Box NextLeft)
     {
         if (Left == null && isActiveLeft)
+        {
             Left = NextLeft;
+            NextLeft.Right = this;
+            isActiveLeft = !isActiveLeft;
+            NextLeft.isActiveRight = !NextLeft.isActiveRight;
+        }
     }
 
     public void AddRight(Box NextRight)
     {
         if (Right == null && isActiveRight)
+        {
             Right = NextRight;
+            NextRight.Left = this;
+            isActiveRight = !isActiveRight;
+            NextRight.isActiveLeft = !NextRight.isActiveLeft;
+        }
     }
 
     public void AddBottom(Box NextBottom)
     {
         if (Bottom == null && isActiveBottom)
+        {
             Bottom = NextBottom;
+            NextBottom.Top = this;
+            isActiveBottom = !isActiveBottom;
+            NextBottom.isActiveTop = !NextBottom.isActiveTop;
+        }
     }
 
 }
--- Assets/Script/DrawMap.cs
+++ Assets/Script/DrawMap.cs
@@ -9,9 +9,17 @@ public class DrawMap : MonoBehaviour
     public int Size = 5;
     public Box BoxMap;
     //public GameObject Cube;
-    private Box MainCopy; 
+    public Box[][] ListBox;
+
+    public Box[] GameWays;
+
     void Start()
     {
+        ListBox = new Box[Size][];
+        for (int i = 0; i < Size; i++)
+        {
+            ListBox[i] = new Box[Size];
+        }
         RamdomPosition(BoxMap);
         CreateMap();
 
@@ -20,108 +28,63 @@ public class DrawMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void CreateMap()
     {
-        if(BoxMap.Top == null)
-        {
-            BoxMap.Top = AddTop(BoxMap, Size);
-        }
-        //if (BoxMap.Bottom == null)
-        //{
-        //    Box Bottom = Instantiate<Box>(BoxMap);
-        //    Bottom.Root.transform.position += Vector3.back * 1;
-        //    BoxMap.Bottom = Bottom;
-        //}
-        //if (BoxMap.Left == null)
-        //{
-        //    Box Left = Instantiate<Box>(BoxMap);
-        //    Left.Root.transform.position += Vector3.left * 1;
-        //    BoxMap.Left = Left;
-        //}
-        //if (BoxMap.Right == null)
-        //{
-        //    Box Right = Instantiate<Box>(BoxMap);
-        //    Right.Root.transform.position += Vector3.right * 1;
-        //    BoxMap.Right = Right;
-        //}
-    }
-
-
-    public Box AddTop(Box Root, int Count)
-    {
-        if(Count == 0)
-        {
-            Root.Left = AddLeft(Root, 5);
-            return Root;
-        }
-        else
-        {
-            Box Top = Instantiate<Box>(Root);
-            Top.transform.position += Vector3.forward * 1;
-            Top.Bottom = Root;
-            return AddTop(Top, Count - 1);
-        }
-            
-    }
-
-    public Box AddLeft(Box Root, int Count)
-    {
-        if (Count == 0)
-        {
-            Root.Bottom = AddBottom(Root, 5);
-            return Root;
-        }
-        else
-        {
-            Box Left = Instantiate<Box>(Root);
-            Left.transform.position += Vector3.left * 1;
-            Left.Right = Root;
-            return AddLeft(Left, Count - 1);
-        }
+        MatrixBox(Size, BoxMap);
 
     }
 
-    public Box AddRight(Box Root, int Count)
+    public void MatrixBox(int n, Box box)
     {
-        if (Count == 0)
+        for (int x = 0; x < n; x++)
         {
-            return Root;
-        }
-        else
-        {
-            Box Right = Instantiate<Box>(Root);
-            Right.transform.position += Vector3.right * 1;
-            Right.Left = Root;
-            return AddRight(Right, Count - 1);
-        }
+            for (int y = 0; y < n; y++)
+            {
+                Box Next = Instantiate<Box>(box);
+                Next.transform.position = new Vector3(x, 0, y);
+                Next.name = x + "," + y;
+                Next.transform.SetParent(transform);
 
+                ListBox[x][y] = Next;
+                if (y != 0)
+                {
+                    Next.AddLeft(ListBox[x][y - 1]);
+                }
+
+                if (x != 0)
+                {
+                    Next.AddTop(ListBox[x - 1][y]);
+                }
+
+            }
+        }
     }
 
-    public Box AddBottom(Box Root, int Count)
+
+    public void AddBox()
     {
-        if (Count == 0)
-        {
-            Root.Right = AddRight(Root, 5);
-            return Root;
-        }
-        else
-        {
-            Box Bot = Instantiate<Box>(Root);
-            Bot.transform.position += Vector3.back * 1;
-            Bot.Top = Root;
-            return AddBottom(Bot, Count - 1);
-        }
+        System.Random random = new System.Random();
+
 
     }
 
     private void RamdomPosition(Box Root)
     {
-        System.Random random = new System.Random();
-        Root.transform.position = new Vector3(random.Next(1,10), random.Next(1, 10), Root.Root.transform.position.z);
+
+
     }
 
+    private void GetFirstRoot()
+    {
+        System.Random random = new System.Random();
+        int x = random.Next(4, 12);
+        int[] lstIdex = { 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        if (x >= 7 || x > 12)
+        {
 
+        }
+    }
 }
